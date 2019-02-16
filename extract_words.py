bf0b3800064570aeeccf81ebee74339794d74c78
JButler333 <31519701+JButler333@users.noreply.github.com>
2019-02-15 18:41:50 -0600

    Add files via upload

--- extract_words.py
+++ extract_words.py
@@ -1,67 +1,49 @@
 import re
+#load in the sherclock holmes text file
+with open("SherlockHolmes.txt", "r") as SherHolmes:
+  text = SherHolmes.read()
 
-def allWords(Book, All):
-  #Open and read the Gutenberg novel
-  with open(Book, "r") as SherHolmes:
-    text = SherHolmes.read()
+#clean the data using the regular expression library
+outText = re.findall(r'[a-zA-Z]+', text)
 
-  #Find all alphabetic words using regular expressions and append the lowercase to a list.
-  outText = re.findall(r'[a-zA-Z]+', text)
-  temp = []
-  for item in outText: 
-      temp.append(item.lower())
-  outText = temp
-  
-  #Create an output string joined by newline characters
-  outString = "\n".join(outText)
+#lowercase each word using a temp variable to store the lowercased words
+temp = []
+for item in outText: 
+    temp.append(item.lower())
+outText = temp
+outString = "\n".join(outText)
 
-  #Write all words to a text file in the correct format.
-  with open(All,"w") as allWords:
-    allWords.write(outString)
+#create a dictionary of all the words and the number of occurances
+wordDict = {}
+for item in outText:
+	if item in wordDict:
+		wordDict[item] = wordDict.get(item)+1
+	else:
+		wordDict[item] = 1
 
-  return outText
+uniqueWords = ""
+#create a dictionary of the frequencies to take the frequencies of the frequencies
+freqDict = {}
+for item in wordDict:
+	if wordDict[item] == 1: #appending unique words to the unique words string to later print
+		uniqueWords = uniqueWords + "\n" + item
+	if wordDict[item] in freqDict: #adding elements to library and indexing the number of occurances
+		freqDict[wordDict[item]] = freqDict.get(wordDict[item]) + 1
+	else:
+		freqDict[wordDict[item]] = 1
 
+#run through frequency dictuonary to append the word frequencies to print to file
+frequency = ""
+for item in freqDict:
+	frequency = frequency + str(item) + ": " + str(freqDict[item]) + "\n"
 
-def Unique_and_Frequency(Book, outText, Unique, Frequencies):
-  #Create a dictionary and get the word frequencies
-  wordDict = {}
-  for item in outText:
-    if item in wordDict:
-      wordDict[item] = wordDict.get(item)+1
-    else:
-      wordDict[item] = 1
-
-  #Create a dictionary and get the unique words, as well as getting the frequencies of each items
-  uniqueWords = ""
-  freqDict = {}
-  for item in wordDict:
-    if wordDict[item] == 1:
-      uniqueWords = uniqueWords + "\n" + item
-    if wordDict[item] in freqDict:
-      freqDict[wordDict[item]] = freqDict.get(wordDict[item]) + 1
-    else:
-      freqDict[wordDict[item]] = 1
-
-  #Write unique words to the correct file in the correct format
-  with open(Unique,"w") as uniqueWords:
-    uniqueWords.write(uniqueWords)
-
-  #Get the frequency in a string format to be appended to the text file
-  frequency = ""
-  for item in freqDict:
-    frequency = frequency + str(item) + ": " + str(freqDict[item]) + "\n"
-
-  #Write the frequencies to the correct file with the correct format 
-  with open(Frequencies,"w") as wordFrequency:
-    wordFrequency.write(frequency)
+#print string to respective text files
+with open("allWords.txt","w") as allWords:
+	allWords.write(outString)
+with open("uniqueWords.txt","w") as uniqueWords:
+	uniqueWords.write(outString)
+with open("wordFrequency.txt","w") as wordFrequency:
+	wordFrequency.write(outString)
 
   
   
-  
-def main():
-
-  outText = allWords("SherlockHolmes.txt", "allWords.txt")
-
-  Unique_and_Frequency("SherlockHolmes.txt", outText, "uniqueWords.txt", "wordFrequency.txt")
-
-main()
