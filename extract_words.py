c8c5efad947c190ae33a592e5ca575352b29c652
Clarice Norton <40498202+TtlGIS@users.noreply.github.com>
2019-02-15 18:21:22 -0600

    Add files via upload

--- extract_words.py
+++ extract_words.py
@@ -1,46 +1,28 @@
 import re
 
-with open("SherlockHolmes.txt", "r") as SherHolmes:
-  text = SherHolmes.read()
+def get_AllWords(FileName, OutFileName):
+    with open(FileName, "r") as SherHolmes:
+    text = SherHolmes.read()
 
-outText = re.findall(r'[a-zA-Z]+', text)
-temp = []
-for item in outText: 
-    temp.append(item.lower())
+    outText = re.findall(r'[a-zA-Z]+', text)
+    temp = []
+    for item in outText: 
+        temp.append(item.lower())
+    outText = temp
 
-outText = temp
-wordDict = {}
-for item in outText:
-	if item in wordDict:
-		wordDict[item] = wordDict.get(item)+1
-	else:
-		wordDict[item] = 1
-
-uniqueWords = ""
-freqDict = {}
-for item in wordDict:
-	if wordDict[item] == 1:
-		uniqueWords = uniqueWords + "\n" + item
-	if wordDict[item] in freqDict:
-		freqDict[wordDict[item]] = freqDict.get(wordDict[item]) + 1
-	else:
-		freqDict[wordDict[item]] = 1
-
-frequency = ""
-for item in freqDict:
-	frequency = frequency + str(item) + ": " + str(freqDict[item]) + "\n"
+    outString = "\n".join(outText)
 
 
+    with open(outFileName,"w") as AllWords:
+      AllWords.write(outString)
 
-outString = "\n".join(outText)
+
+def main():
+
+    get_allWords("SherlockHolmes.txt", "AllWords.txt")
+
+main()
+      
 
 
-with open("allWords.txt","w") as allWords:
-	allWords.write(outString)
-with open("uniqueWords.txt","w") as uniqueWords:
-	uniqueWords.write(outString)
-with open("wordFrequency.txt","w") as wordFrequency:
-	wordFrequency.write(outString)
-
-  
   
